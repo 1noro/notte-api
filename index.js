f63f83aeabf58669ba37fc0ea1d8e71402f27e51
1noro <ppuubblliicc@protonmail.com>
2022-03-01 19:21:45 +0100

    feat: :sparkles: implementado el endpoint /health/live

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('Hello World');
+const { json } = require("express");
+const express = require("express");
+const app = express();
+
+const port = 3000;
+const appName = "Notte API";
+
+app.get('/v1/health/live', (req, res) => {
+  res.json({
+    "alive": true,
+    "message": "The server is alive."
+  });
+});
+
+app.listen(port, () => console.log(`${appName} listening on port ${port}!`));
